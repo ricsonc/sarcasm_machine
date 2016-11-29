5fe56e9f9ce947d229b9437f5ee8346b5290f305
Ricson Cheng <ricsonc@andrew.cmu.edu>
2016-11-29 18:07:23 -0500

    firxed grscip

---
+++ data_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+
+import numpy as np
+import random
+
+def add_comp(vec):
+    return np.append(vec, [1])
+
+def add_dim(sample):
+    return map(add_comp, sample)
+
+def resize(n):
+    def do_resize(sample):
+        if len(sample) >= n:
+            return sample[:n]
+        if len(sample) < n:
+            return np.concatenate(
+                (sample,np.zeros((n-len(sample),len(sample[0]))))
+            )
+    return do_resize
+
+def stack(Xslist):
+    return np.array(map(np.concatenate,(zip(*Xslist))))
+    
+def load_files(Xfile_list, yfile, n = 20):
+    Xslist = []
+    for file in Xfile_list:
+        Xs = np.load(file)
+        for x in Xs:
+            if not len(x):
+                print file
+        Xs = map(add_dim, Xs)
+        Xs = np.array(map(resize(n), Xs))
+        Xslist.append(Xs)
+    return (stack(Xslist), np.load(yfile))
+
+load_files(['corpus_list.npy',
+            'corpus_list_bigram.npy',
+            'corpus_list_trigram.npy',
+            'corpus_list_polarity.npy'],
+           'label_list.npy',
+           300)
--- script_delete_zero.py
+++ script_delete_zero.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python2
+
+
 import numpy as np
 corpus = np.load('corpus_list.npy')
 corpus_bigram = np.load('corpus_list_bigram.npy')
@@ -9,22 +12,31 @@ for i in xrange(len(corpus)):
 	if len(corpus[i])==0:
 		wrong = i
 
-np.delete(corpus,wrong,0)
-np.delete(corpus_pol,wrong)
-np.delete(label,wrong)
+print len(corpus),len(label)
+corpus = np.delete(corpus,wrong,0)
+corpus_pol = np.delete(corpus_pol,wrong)
+label = np.delete(label,wrong)
 print len(corpus),len(label)
 
+for c in corpus:
+        if len(c) == 0:
+                print '???'
+
+'''
 for i in xrange(len(corpus_bigram)):
 	if len(corpus[i])==0:
 		wrong = i
+'''
 
-np.delete(corpus_bigram,wrong,0)
+corpus_bigram = np.delete(corpus_bigram,wrong,0)
 
+'''
 for i in xrange(len(corpus_trigram)):
 	if len(corpus[i])==0:
 		wrong = i
+'''
 
-np.delete(corpus_trigram,wrong,0)
+corpus_trigram = np.delete(corpus_trigram,wrong,0)
 
 np.save('corpus_list',corpus)
 np.save('label_list',label)
